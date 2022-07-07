33bb56e04293908d6806926c6b9bddc8d6a6ee32
Alexr16 <j.alex-1996@hotmail.com>
2022-07-07 12:29:40 -0500

    Initial Commit

---
+++ Shared/ScrudimgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrudimgerApp.swift
+//  Shared
+//
+//  Created by Jorge Rios Revelo on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrudimgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
