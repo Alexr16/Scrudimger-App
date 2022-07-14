749c12e1736bcf5eb3bf5dd20c1ae2f4d1e4ab1e
Alexr16 <j.alex-1996@hotmail.com>
2022-07-14 10:38:39 -0500

    Set scrudimger views

--- Shared/ScrudimgerApp.swift
+++ Shared/ScrudimgerApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct ScrudimgerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ScrumsView(scrums: DailyScrum.sampleData)
+            }
         }
     }
 }
